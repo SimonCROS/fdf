d0a2a48bd6e1e934f5a3f52bb72bd18013097b6a
SimonCROS <simon.dc.cros@gmail.com>
2021-05-31 00:48:12 +0200

    Patch sides

--- src/engine/renderer.c
+++ src/engine/renderer.c
@@ -9,14 +9,16 @@ int	render(t_vars *vars)
 
 	map = vars->map;
 	y = 0;
-	while (y < map->height - 1)
+	while (y < map->height)
 	{
 		x = 0;
-		while (x < map->width - 1)
+		while (x < map->width)
 		{
 			vertex = get_vertex(map, x, y);
-			project(vars, vertex, get_vertex(map, x + 1, y));
-			project(vars, vertex, get_vertex(map, x, y + 1));
+			if (x < map->width - 1)
+				project(vars, vertex, get_vertex(map, x + 1, y));
+			if (y < map->height - 1)
+				project(vars, vertex, get_vertex(map, x, y + 1));
 			x++;
 		}
 		y++;
