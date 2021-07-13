295cc7f91ca90f2a3d9b3522a61728019b04b4e7
Simon Cros <scros@student.42lyon.fr>
2021-07-13 15:37:42 +0200

    Move same function from pthread_name_ft

--- libft/src/tpool/pthread_name_ft.c
+++ libft/src/tpool/pthread_name_ft.c
@@ -7,19 +7,14 @@ int	pthread_setname_ft(char *name)
 	return (pthread_setname_np(name));
 }
 
-int	pthread_getname_ft(char *name, size_t buffer_len)
-{
-	return (pthread_getname_np(pthread_self(), name, buffer_len));
-}
-
 #elif defined __linux__
 int	pthread_setname_ft(char *name)
 {
 	return (pthread_setname_np(pthread_self(), name));
 }
+#endif
 
 int	pthread_getname_ft(char *name, size_t buffer_len)
 {
 	return (pthread_getname_np(pthread_self(), name, buffer_len));
 }
-#endif
